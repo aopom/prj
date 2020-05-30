9fcd6a7082326b1875130ba08e707100746e4563
Joris P <joris@placette.fr>
2020-05-30 17:24:14 +0200

    add variable class and other dope things my friend !

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 from lib.gopherpysat import Gophersat
 from lib.wumpus import WumpusWorld
 
+from pprint import pprint
 
+class Variable:
+    
+    def __init__(self, positive, letter, i, j):
+        self.letter = letter
+        self.i = i
+        self.j = j
+        self.positive = positive
+
+    def __str__(self):
+        if self.positive:
+            output = ""
+        else:
+            output = "-"
+
+        return output + "{}_{}_{}".format(self.letter, self.i, self.j)
+    
+    __repr__ = __str__
+    pretty = __str__
+
+    def __neg__(self):
+        return Variable(not self.positive, self.letter, self.i, self.j)
+            
 def print_array_multiline(arraaaay):
-    print()
     for line in arraaaay:
         print(line)
-
 
 def explo_full_cautious(ww):  # coute 800
     for i in range(WORLD_SIZE):
@@ -21,20 +42,33 @@ def explo_full_simple_probe(ww):  # coute 4160
 
 
 def knowledge_to_clauses():
-    clauses = []
+    clauses = [] 
     size = ww.get_n()
     knowledge = ww.get_knowledge()
     for i in range(size):
         for j in range(size):
             if knowledge[i][j] != "?":
                 for letter in "PWBSG":
-                    if letter in knowledge[i][j]:
-                        clauses.append("{}_{}_{}".format(letter, i, j))
-                    else:
-                        clauses.append("-{}_{}_{}".format(letter, i, j))
+                    clauses.append(Variable( (letter in knowledge[i][j]) , letter, i, j))
 
     return clauses
 
+def interrogate(clause):
+    """retourne un booleen
+    """
+    gs.push_pretty_clause(clause)
+    output = gs.solve()
+    gs.pop_clause()
+    return output
+
+def test_variable(variable):
+    # si c' est bon a = 1 et b = 0
+    # si on sait pas a-b = 0
+    # si c' est pas bon a-b = -1
+    a = interrogate([variable.pretty()])
+    b = interrogate([(-variable).pretty()])
+    
+    return a - b
 
 def mainloop():
     # Create world
@@ -49,7 +83,7 @@ def mainloop():
     # Generate Vocabulary
     # Pit, Wumpus, Breeze, Stench, Gold
     voc = [
-        f"{letter}{i}_{j}"
+        f"{letter}_{i}_{j}"
         for i in range(WORLD_SIZE)
         for j in range(WORLD_SIZE)
         for letter in ["P", "W", "B", "S", "G"]
@@ -63,6 +97,7 @@ def mainloop():
 
     for i in range(WORLD_SIZE):
         for j in range(WORLD_SIZE):
+            # if i != 3 and j != 3:
             ww.cautious_probe(i, j)
 
     ww.print_knowledge()
@@ -70,6 +105,14 @@ def mainloop():
     print(knowledge_to_clauses())
     print("cost : {}".format(ww.get_cost()))
 
+    clauses = knowledge_to_clauses()
+    for clause in clauses :
+        # print(clause.pretty())
+        gs.push_pretty_clause([clause.pretty()])
+
+    print(test_variable(Variable(True, "W", 0, 0)))
+    print(test_variable(Variable(True, "W", 3, 3)))
+    print(test_variable(Variable(True, "W", 2, 0)))
     # print(ww)
 
     # # Create gophersat object

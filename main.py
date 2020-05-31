981db6fb576998d339a4b055a9b03b44aeff9b65
Joris P <joris@placette.fr>
2020-05-31 12:10:59 +0200

    separate Variable class, using it form gopherpysat

--- main.py
+++ main.py
@@ -1,27 +1,6 @@
 from lib.gopherpysat import Gophersat
 from lib.wumpus import WumpusWorld
-
-
-class Variable:
-    def __init__(self, positive, letter, i, j):
-        self.letter = letter
-        self.i = i
-        self.j = j
-        self.positive = positive
-
-    def __str__(self):
-        if self.positive:
-            output = ""
-        else:
-            output = "-"
-        return output + "{}_{}_{}".format(self.letter, self.i, self.j)
-
-    __repr__ = __str__
-    pretty = __str__
-
-    def __neg__(self):
-        return Variable(not self.positive, self.letter, self.i, self.j)
-
+from lib.variable import *
 
 def beauty_print(double_array):
     print("[")

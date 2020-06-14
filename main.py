e7de92f67e3708176f4077f1a15a46239c2cf1ce
Paco <>
2020-06-14 03:30:44 +0200

    Threads!

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import random
+import threading
+import multiprocessing
 from lib.gopherpysat import Gophersat
 from lib.wumpus import WumpusWorld
 
@@ -56,19 +58,20 @@ def fill_rules():
         [f"-S_0_0"],
         [f"-B_0_0"],
     ]"""
+    game_rules = []
     almeno_uno_wumpus = []
     for i in range(WORLD_SIZE):
         for j in range(WORLD_SIZE):
             almeno_uno_wumpus.append(f"W_{i}_{j}")
 
-    gs.push_pretty_clause(almeno_uno_wumpus)
+    game_rules.append(almeno_uno_wumpus)
 
     for i in range(WORLD_SIZE):
         for j in range(WORLD_SIZE):
             # One thing per tile
-            gs.push_pretty_clause([f"-G_{i}_{j}", f"-W_{i}_{j}"])
-            gs.push_pretty_clause([f"-G_{i}_{j}", f"-P_{i}_{j}"])
-            gs.push_pretty_clause([f"-P_{i}_{j}", f"-W_{i}_{j}"])
+            game_rules.append([f"-G_{i}_{j}", f"-W_{i}_{j}"])
+            game_rules.append([f"-G_{i}_{j}", f"-P_{i}_{j}"])
+            game_rules.append([f"-P_{i}_{j}", f"-W_{i}_{j}"])
 
             # One Wumpus per game
             # for i in range(WORLD_SIZE):
@@ -78,7 +81,7 @@ def fill_rules():
                     # if i != k or j != l:
                     # 10% plus rapide xD à l'éxecution
                     if i < k or j < l:
-                        gs.push_pretty_clause([f"-W_{i}_{j}", f"-W_{k}_{l}"])
+                        game_rules.append([f"-W_{i}_{j}", f"-W_{k}_{l}"])
 
             # Pits around the Breeze
             # for i in range(WORLD_SIZE):
@@ -92,7 +95,7 @@ def fill_rules():
                 clause.append(f"P_{i + 1}_{j}")
             if j < WORLD_SIZE - 1:
                 clause.append(f"P_{i}_{j + 1}")
-            gs.push_pretty_clause(clause)
+            game_rules.append(clause)
 
             # Wumpus around the Strench
             # for i in range(WORLD_SIZE):
@@ -106,33 +109,33 @@ def fill_rules():
                 clause.append(f"W_{i + 1}_{j}")
             if j < WORLD_SIZE - 1:
                 clause.append(f"W_{i}_{j + 1}")
-            gs.push_pretty_clause(clause)
+            game_rules.append(clause)
 
             # un puit est entouré de environ 4 breeze sauf sur les bords
             # for i in range(WORLD_SIZE):
             # for j in range(WORLD_SIZE):
             clause = [f"-P_{i}_{j}"]
             if i > 0:
-                gs.push_pretty_clause(clause + [f"B_{i - 1}_{j}"])
+                game_rules.append(clause + [f"B_{i - 1}_{j}"])
             if j > 0:
-                gs.push_pretty_clause(clause + [f"B_{i}_{j - 1}"])
+                game_rules.append(clause + [f"B_{i}_{j - 1}"])
             if i < WORLD_SIZE - 1:
-                gs.push_pretty_clause(clause + [f"B_{i + 1}_{j}"])
+                game_rules.append(clause + [f"B_{i + 1}_{j}"])
             if j < WORLD_SIZE - 1:
-                gs.push_pretty_clause(clause + [f"B_{i}_{j + 1}"])
+                game_rules.append(clause + [f"B_{i}_{j + 1}"])
 
             # un wumpus est entouré de environ 4 strench sauf sur les bords
             # for i in range(WORLD_SIZE):
             # for j in range(WORLD_SIZE):
             clause = [f"-W_{i}_{j}"]
             if i > 0:
-                gs.push_pretty_clause(clause + [f"S_{i - 1}_{j}"])
+                game_rules.append(clause + [f"S_{i - 1}_{j}"])
             if j > 0:
-                gs.push_pretty_clause(clause + [f"S_{i}_{j - 1}"])
+                game_rules.append(clause + [f"S_{i}_{j - 1}"])
             if i < WORLD_SIZE - 1:
-                gs.push_pretty_clause(clause + [f"S_{i + 1}_{j}"])
+                game_rules.append(clause + [f"S_{i + 1}_{j}"])
             if j < WORLD_SIZE - 1:
-                gs.push_pretty_clause(clause + [f"S_{i}_{j + 1}"])
+                game_rules.append(clause + [f"S_{i}_{j + 1}"])
 
             # pas d'odeur donc pas de wumpus autour
             # for i in range(WORLD_SIZE):
@@ -140,33 +143,33 @@ def fill_rules():
             clause = [f"S_{i}_{j}"]
             # clause = [f"S_{i}_{j}", f"P_{i}_{j}"]
             if i > 0:
-                gs.push_pretty_clause(clause + [f"-W_{i - 1}_{j}"])
+                game_rules.append(clause + [f"-W_{i - 1}_{j}"])
             if j > 0:
-                gs.push_pretty_clause(clause + [f"-W_{i}_{j - 1}"])
+                game_rules.append(clause + [f"-W_{i}_{j - 1}"])
             if i < WORLD_SIZE - 1:
-                gs.push_pretty_clause(clause + [f"-W_{i + 1}_{j}"])
+                game_rules.append(clause + [f"-W_{i + 1}_{j}"])
             if j < WORLD_SIZE - 1:
-                gs.push_pretty_clause(clause + [f"-W_{i}_{j + 1}"])
+                game_rules.append(clause + [f"-W_{i}_{j + 1}"])
 
             # pas de vent donc pas de puit autour
             # for i in range(WORLD_SIZE):
             # for j in range(WORLD_SIZE):
             clause = [f"B_{i}_{j}", f"W_{i}_{j}"]
             if i > 0:
-                gs.push_pretty_clause(clause + [f"-P_{i - 1}_{j}"])
+                game_rules.append(clause + [f"-P_{i - 1}_{j}"])
             if j > 0:
-                gs.push_pretty_clause(clause + [f"-P_{i}_{j - 1}"])
+                game_rules.append(clause + [f"-P_{i}_{j - 1}"])
             if i < WORLD_SIZE - 1:
-                gs.push_pretty_clause(clause + [f"-P_{i + 1}_{j}"])
+                game_rules.append(clause + [f"-P_{i + 1}_{j}"])
             if j < WORLD_SIZE - 1:
-                gs.push_pretty_clause(clause + [f"-P_{i}_{j + 1}"])
+                game_rules.append(clause + [f"-P_{i}_{j + 1}"])
 
-    # return game_rules
+    return game_rules
 
 
 def knowledge_to_clauses():
     clauses = []
-    size = ww.get_n()
+    size = WORLD_SIZE
     knowledge = ww.get_knowledge()
     for i in range(size):
         for j in range(size):
@@ -179,12 +182,12 @@ def knowledge_to_clauses():
     return clauses
 
 
-def interrogate(clause):
+def interrogate(gopherpysat, clause):
     global interrogation_count
     interrogation_count += 1
-    gs.push_pretty_clause(clause)
-    output = gs.solve()
-    gs.pop_clause()
+    gopherpysat.push_pretty_clause(clause)
+    output = gopherpysat.solve()
+    gopherpysat.pop_clause()
     return output
 
 
@@ -193,11 +196,11 @@ def test_variable(variable):
     # si on sait pas a-b = 0
     # si c' est pas bon a-b = -1
 
-    a = interrogate([variable])
+    a = interrogate(gs, [variable])
     if variable[0] == "-":
-        b = interrogate([variable[1:]])
+        b = interrogate(gs, [variable[1:]])
     else:
-        b = interrogate([f"-{variable}"])
+        b = interrogate(gs, [f"-{variable}"])
 
     return a - b
 
@@ -208,67 +211,96 @@ def safe_tile(i, j):
     return 1 == test_variable(f"-W_{i}_{j}") and 1 == test_variable(f"-P_{i}_{j}")
 
 
-def safe_tile2(i, j):
+def safe_tile2(gopherpysat, i, j):
     # Are we sure there is a pit ?
-    there_is_a_pit = 0 == interrogate([f"-P_{i}_{j}"])
+    there_is_a_pit = 0 == interrogate(gopherpysat, [f"-P_{i}_{j}"])
     if there_is_a_pit:
         return -1
 
     # Are we sure there is a wumpus ?
-    there_is_a_wumpus = 0 == interrogate([f"-W_{i}_{j}"])
+    there_is_a_wumpus = 0 == interrogate(gopherpysat, [f"-W_{i}_{j}"])
     if there_is_a_wumpus:
         return -1
 
     # Are we sure there is no wumpus ?
-    there_is_no_wumpus = 0 == interrogate([f"W_{i}_{j}"])
+    there_is_no_wumpus = 0 == interrogate(gopherpysat, [f"W_{i}_{j}"])
     if not there_is_no_wumpus:
         return 0
 
     # Are we sure there is a pit ?
-    there_is_no_pit = 0 == interrogate([f"P_{i}_{j}"])
+    there_is_no_pit = 0 == interrogate(gopherpysat, [f"P_{i}_{j}"])
     return there_is_no_pit
 
 
-def explo_full_gopherpysat2():
+def adjacent_to_known_tile(knowledge, i, j):
+    return (
+        (i > 0 and knowledge[i - 1][j] != "?")
+        or (j > 0 and knowledge[i][j - 1] != "?")
+        or (i < WORLD_SIZE - 1 and knowledge[i + 1][j] != "?")
+        or (j < WORLD_SIZE - 1 and knowledge[i][j + 1] != "?")
+    )
+
+
+def try_multiple_tiles(gopherpysat, tiles, start, step, knowledge):
+    global action
+
+    for clause in knowledge:
+        gopherpysat.push_pretty_clause(clause)
+
+    index = start
+    while index < len(tiles):
+        (i, j) = tiles[index]
+        index += step
+
+        safe = safe_tile2(gopherpysat, i, j)
+        if safe == 1:
+            action = True
+            ww.probe(i, j)
+            print(f"ninja probe {i} {j}")
+        elif safe == -1:
+            action = True
+            ww.cautious_probe(i, j)
+            print(f"sure cautious probe {i} {j}")
+
+
+def explo_full_gopherpysat2(gopherpysats):
     # première action
     ww.probe(0, 0)
     unknown_tiles = [(i, j) for i in range(WORLD_SIZE) for j in range(WORLD_SIZE)]
     # While some tiles are unknown
     while len(unknown_tiles) > 0:
-        # add new knowledge
-        for clause in knowledge_to_clauses():
-            gs.push_pretty_clause(clause)
-        # pick a tile
+        # select all unknown tiles
         knowledge = ww.get_knowledge()
-        len_knowledge = len(knowledge)
         unknown_tiles = [(i, j) for (i, j) in unknown_tiles if knowledge[i][j] == "?"]
 
-        action = False
-        for (i, j) in unknown_tiles:
-            safe = safe_tile2(i, j)
-            if safe == 1:
-                action = True
-                ww.probe(i, j)
-                print("ninja probe {} {}".format(i, j))
-            elif safe == -1:
-                action = True
-                ww.cautious_probe(i, j)
-                print("sure cautious probe {} {}".format(i, j))
+        # choose only tiles worth trying
+        peripheric_tiles = [(i, j) for (i, j) in unknown_tiles if adjacent_to_known_tile(knowledge, i, j)]
 
-        # probe
-        # ninja = False
-        # if len(safe_tiles) > 0:
-        # ninja = True
-        # for (i, j) in safe_tiles:
-        # ww.probe(i, j)
-        # print("ninja probe {} {}".format(i, j))
-        #
-        # if len(unsafe_tiles) > 0:
-        # for (i, j) in unsafe_tiles:
-        # ww.cautious_probe(i, j)
-        # print("sure cautious probe {} {}".format(i, j))
-        #
-        # elif not ninja and len(unknown_tiles) > 0:
+        # split in pools for the gopherpysats
+        # splited_groups = [[] for i in range(len(gopherpysats))]
+        # go_number = 0
+        # for i in range(len(peripheric_tiles)):
+        #    go_number = (go_number + 1) % len(gopherpysats)
+        #    splited_groups[go_number].append(peripheric_tiles[i])
+
+        # prepare knowledge to be added to each gopherpysat
+        knowledge = knowledge_to_clauses()
+
+        # launch threads
+        threads = []
+        global action
+        action = False
+
+        for i in range(len(gopherpysats)):
+            thread = threading.Thread(target=try_multiple_tiles, args=(gopherpysats[i], peripheric_tiles, i, len(gopherpysats), knowledge))
+            threads.append(thread)
+            thread.start()
+
+        # wait for threads
+        for thread in threads:
+            thread.join()
+
+        # if no tile was probed
         if not action and len(unknown_tiles) > 0:
             (i, j) = unknown_tiles[random.randrange(len(unknown_tiles))]
             ww.cautious_probe(i, j)
@@ -281,7 +313,7 @@ def mainloop():
     # Create world
     global ww
     # We provide a world length and a seed
-    ww = WumpusWorld(n=8, seed=14)
+    ww = WumpusWorld(n=10, seed=11)
     # print(ww)
     # print("cost : {}".format(ww.get_cost()))
 
@@ -294,19 +326,18 @@ def mainloop():
     voc = [f"{letter}_{i}_{j}" for i in range(WORLD_SIZE) for j in range(WORLD_SIZE) for letter in ["P", "W", "B", "S", "G"]]
 
     # Create gophersat object
-    global gs
-    gs = Gophersat(voc=voc)
+    cpus = multiprocessing.cpu_count()
+    gopherpysats = [Gophersat(voc=voc) for i in range(cpus)]
+    print(f"cpus: {cpus}")
 
-    # global game_rules
-    # game_rules = fill_rules()
-    # for clause in game_rules:
-    #     gs.push_pretty_clause(clause)
-    fill_rules()
+    for clause in fill_rules():
+        for gs in gopherpysats:
+            gs.push_pretty_clause(clause)
 
     global interrogation_count
     interrogation_count = 0
 
-    explo_full_gopherpysat2()
+    explo_full_gopherpysat2(gopherpysats)
     print("cost : {}".format(ww.get_cost()))
     print("interrogation_count : {}".format(interrogation_count))
     ww.print_knowledge()

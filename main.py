99289bde3af01e2ff7b525d8011c3c36fcb44e99
Paco <>
2020-06-14 03:40:16 +0200

    cleaned up a lot of dead code

--- main.py
+++ main.py
@@ -12,52 +12,7 @@ def beauty_print(double_array):
     print("]")
 
 
-def explo_full_cautious():  # coute 800
-    for i in range(WORLD_SIZE):
-        for j in range(WORLD_SIZE):
-            ww.cautious_probe(i, j)
-
-
-def explo_full_simple_probe():  # coute 4160
-    for i in range(WORLD_SIZE):
-        for j in range(WORLD_SIZE):
-            ww.probe(i, j)
-
-
-def explo_full_gopherpysat():
-    # première action
-    ww.probe(0, 0)
-    unknown_tiles = [":troll_face:"]
-    # While some tiles are unknown
-    while len(unknown_tiles) > 0:
-        # add new knowledge
-        for clause in knowledge_to_clauses():
-            gs.push_pretty_clause(clause)
-        # pick a tile
-        knowledge = ww.get_knowledge()
-        len_knowledge = len(knowledge)
-        unknown_tiles = [(i, j) for i in range(len_knowledge) for j in range(len_knowledge) if knowledge[i][j] == "?"]
-        safe_tiles = [(i, j) for (i, j) in unknown_tiles if safe_tile(i, j)]
-        # probe
-        if len(safe_tiles) > 0:
-            for (i, j) in safe_tiles:
-                ww.probe(i, j)
-                print("ninja probe {} {}".format(i, j))
-        elif len(unknown_tiles) > 0:
-            (i, j) = unknown_tiles[random.randrange(len(unknown_tiles))]
-            ww.cautious_probe(i, j)
-            print("cautious probe {} {}".format(i, j))
-        # ww.print_knowledge()
-
-
 def fill_rules():
-    # First tile rule
-    """game_rules = [
-        [f"-W_0_0"],
-        [f"-P_0_0"],
-        [f"-S_0_0"],
-        [f"-B_0_0"],
-    ]"""
     game_rules = []
     almeno_uno_wumpus = []
     for i in range(WORLD_SIZE):
@@ -191,42 +146,19 @@ def interrogate(gopherpysat, clause):
     return output
 
 
-def test_variable(variable):
-    # si c' est bon a = 1 et b = 0
-    # si on sait pas a-b = 0
-    # si c' est pas bon a-b = -1
-
-    a = interrogate(gs, [variable])
-    if variable[0] == "-":
-        b = interrogate(gs, [variable[1:]])
-    else:
-        b = interrogate(gs, [f"-{variable}"])
-
-    return a - b
-
-
-def safe_tile(i, j):
-    """should you probe this tile without cautious_probe ?
-    """
-    return 1 == test_variable(f"-W_{i}_{j}") and 1 == test_variable(f"-P_{i}_{j}")
-
-
 def safe_tile2(gopherpysat, i, j):
     # Are we sure there is a pit ?
     there_is_a_pit = 0 == interrogate(gopherpysat, [f"-P_{i}_{j}"])
     if there_is_a_pit:
         return -1
-
     # Are we sure there is a wumpus ?
     there_is_a_wumpus = 0 == interrogate(gopherpysat, [f"-W_{i}_{j}"])
     if there_is_a_wumpus:
         return -1
-
     # Are we sure there is no wumpus ?
     there_is_no_wumpus = 0 == interrogate(gopherpysat, [f"W_{i}_{j}"])
     if not there_is_no_wumpus:
         return 0
-
     # Are we sure there is a pit ?
     there_is_no_pit = 0 == interrogate(gopherpysat, [f"P_{i}_{j}"])
     return there_is_no_pit
@@ -276,13 +208,6 @@ def explo_full_gopherpysat2(gopherpysats):
         # choose only tiles worth trying
         peripheric_tiles = [(i, j) for (i, j) in unknown_tiles if adjacent_to_known_tile(knowledge, i, j)]
 
-        # split in pools for the gopherpysats
-        # splited_groups = [[] for i in range(len(gopherpysats))]
-        # go_number = 0
-        # for i in range(len(peripheric_tiles)):
-        #    go_number = (go_number + 1) % len(gopherpysats)
-        #    splited_groups[go_number].append(peripheric_tiles[i])
-
         # prepare knowledge to be added to each gopherpysat
         knowledge = knowledge_to_clauses()
 
@@ -314,9 +239,6 @@ def mainloop():
     global ww
     # We provide a world length and a seed
     ww = WumpusWorld(n=10, seed=11)
-    # print(ww)
-    # print("cost : {}".format(ww.get_cost()))
-
     # World width
     global WORLD_SIZE
     WORLD_SIZE = ww.get_n()
@@ -343,101 +265,5 @@ def mainloop():
     ww.print_knowledge()
 
 
-def test_gamerules():
-    # Create world
-    global ww
-    ww = WumpusWorld()
-    print(ww)
-    print("cost : {}".format(ww.get_cost()))
-
-    # World width
-    global WORLD_SIZE
-    WORLD_SIZE = ww.get_n()
-
-    # Generate Vocabulary
-    # Pit, Wumpus, Breeze, Stench, Gold
-    voc = [f"{letter}_{i}_{j}" for i in range(WORLD_SIZE) for j in range(WORLD_SIZE) for letter in ["P", "W", "B", "S", "G"]]
-
-    # Create gophersat object
-    global gs
-    gs = Gophersat(voc=voc)
-
-    global game_rules
-    game_rules = fill_rules()
-    for clause in game_rules:
-        gs.push_pretty_clause(clause)
-
-    assert gs.solve() == True
-
-    ww.probe(0, 0)  # première action
-    for clause in knowledge_to_clauses():
-        gs.push_pretty_clause(clause)
-    ww.print_knowledge()
-
-    print("beauty_print(game_rules)")
-    beauty_print(game_rules)
-    print("beauty_print(knowledge_to_clauses())")
-    beauty_print(knowledge_to_clauses())
-
-    # on ne sait pas ou le wumpus est ni les pits
-    for i in range(WORLD_SIZE):
-        for j in range(WORLD_SIZE):
-            if (i, j) != (0, 0) and (i, j) != (0, 1) and (i, j) != (1, 0):
-                for letter in "WPBS":
-                    # on ne sait rien  sur ces cases là!
-                    assert test_variable(Variable(True, letter, i, j)) == 0
-                    assert test_variable(Variable(False, letter, i, j)) == 0
-            elif (i, j) == (0, 0):
-                for letter in "WPBS":
-                    # Y' a R
-                    assert test_variable(Variable(True, letter, i, j)) == -1
-                    assert test_variable(Variable(False, letter, i, j)) == 1
-            elif (i, j) == (0, 1) or (i, j) == (1, 0):
-                # on ne sait pas s' il y a une brise ou odeur pestidentielle
-                print("""test_variable(f"B_{i}_{j}" == 0  """, test_variable(f"B_{i}_{j}"))
-                assert test_variable(f"B_{i}_{j}") == 0
-                assert test_variable(f"-B_{i}_{j}") == 0
-                assert test_variable(f"S_{i}_{j}") == 0
-                assert test_variable(f"-S_{i}_{j}") == 0
-                # il n'y a pas de danger
-                assert test_variable(f"W_{i}_{j}") == -1
-                assert test_variable(f"-W_{i}_{j}") == 1
-                assert test_variable(f"P_{i}_{j}") == -1
-                assert test_variable(f"-P_{i}_{j}") == 1
-
-    # print("game rules:")
-    # beauty_print(game_rules)
-
-    # runtime tests
-    ww.print_knowledge()
-    ww.cautious_probe(2, 0)  # trouve un wumpus
-    for clause in knowledge_to_clauses():
-        gs.push_pretty_clause(clause)
-
-    ww.print_knowledge()
-    for i in range(WORLD_SIZE):
-        for j in range(WORLD_SIZE):
-            if (i, j) != (2, 0):
-                assert test_variable(f"W_{i}_{j}") == -1
-                assert test_variable(f"-W_{i}_{j}") == 1
-            elif (i, j) == (0, 0):
-                for letter in "WPBS":
-                    # Y' a R
-                    assert test_variable(Variable(True, letter, i, j)) == -1
-                    assert test_variable(Variable(False, letter, i, j)) == 1
-            elif (i, j) == (0, 1) or (i, j) == (1, 0):
-                # on ne sait pas s' il y a une brise ou odeur pestidentielle
-                assert test_variable(f"B_{i}_{j}") == 0
-                assert test_variable(f"-B_{i}_{j}") == 0
-                assert test_variable(f"S_{i}_{j}") == 0
-                assert test_variable(f"-S_{i}_{j}") == 0
-
-    # for (i, j) in ((1, 0), (3, 0), (2, 1)):
-    # assert interrogate([f"P_{i}_{j}", f"P_{i}_{j}"]}
-
-    print("cost : {}".format(ww.get_cost()))
-
-
 if __name__ == "__main__":
     mainloop()
-    # test_gamerules()

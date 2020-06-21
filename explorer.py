5c347e52b41af34828a06e6137c82fda548d4d75
JPL <joris.placette@4dviews.com>
2020-06-21 21:17:24 +0200

    ca roule comme sans POO {

--- explorer.py
+++ explorer.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python3
+
 import heapq
 from mapper import Mapper
+import mapper
 
 class SquareGrid:
     def __init__(self, width, height):
@@ -35,46 +38,57 @@ class PriorityQueue:
     def get(self):
         return heapq.heappop(self.elements)[1]
 
-def heuristic(a, b):
-    (x1, y1) = a
-    (x2, y2) = b
-    return abs(x1 - x2) + abs(y1 - y2)
-
-def a_star_search(graph, start, goal):
-    frontier = PriorityQueue()
-    frontier.put(start, 0)
-    came_from = {}
-    cost_so_far = {}
-    came_from[start] = None
-    cost_so_far[start] = 0
-    
-    while not frontier.empty():
-        current = frontier.get()
-        
-        if current == goal:
-            break
-        
-        for next in graph.neighbors(current):
-            new_cost = cost_so_far[current] + 1
-            if next not in cost_so_far or new_cost < cost_so_far[next]:
-                cost_so_far[next] = new_cost
-                priority = new_cost + heuristic(goal, next)
-                frontier.put(next, priority)
-                came_from[next] = current
-    
-    # return came_from, cost_so_far
-    path = []
-    path.append(goal)
-    while (came_from[path[-1]] != None):
-        path.append(came_from[path[-1]])
-    return path[::-1]
-
 class Explorer():
-    def __init__(self):
-        pass
+    def __init__(self, n=10, seed = 42 ,verbose = False):
+        self.WORLD_SIZE = n
+        self.grid = SquareGrid(self.WORLD_SIZE, self.WORLD_SIZE)
+
+
+    def heuristic(self, a, b):
+        (x1, y1) = a
+        (x2, y2) = b
+        return abs(x1 - x2) + abs(y1 - y2)
+
     def run(self):
-        my_mapper = Mapper(n=7, seed=5, verbose=True)
-        my_mapper.main()
+        # my_mapper = Mapper(n=7, seed=5, verbose=True)
+        # my_mapper.main()
+
+        
+        start = (0,0)
+        end = (self.WORLD_SIZE-1, self.WORLD_SIZE-1)
+
+        path = self.a_star_search(start , end)
+        print(path)
+        
+    def a_star_search(self, start, goal):
+        frontier = PriorityQueue()
+        frontier.put(start, 0)
+        came_from = {}
+        cost_so_far = {}
+        came_from[start] = None
+        cost_so_far[start] = 0
+        
+        while not frontier.empty():
+            current = frontier.get()
+            
+            if current == goal:
+                break
+            
+            for next in self.grid.neighbors(current):
+                new_cost = cost_so_far[current] + 1
+                if next not in cost_so_far or new_cost < cost_so_far[next]:
+                    cost_so_far[next] = new_cost
+                    priority = new_cost + self.heuristic(goal, next)
+                    frontier.put(next, priority)
+                    came_from[next] = current
+        
+        # return came_from, cost_so_far
+        path = []
+        path.append(goal)
+        while (came_from[path[-1]] != None):
+            path.append(came_from[path[-1]])
+
+        return path[::-1]
 
 
 
